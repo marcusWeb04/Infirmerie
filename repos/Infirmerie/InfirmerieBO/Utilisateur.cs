6ab0b8b7a640a4b552a31db6815c5528dbaf5f48
Marcus <101703473+marcusWeb04@users.noreply.github.com>
2023-11-09 11:10:52 +0100

    Création de la classe Utilisateur dans la BO

---
+++ repos/Infirmerie/InfirmerieBO/Utilisateur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfirmerieBO
+{
+    public class Utilisateur
+    {
+        //attribut
+        private string nom;
+        private string motDePasse;
+
+        public Utilisateur(string nom, string motDePasse)
+        {
+            this.nom = nom;
+            this.motDePasse = motDePasse;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public string MotDePasse { get => motDePasse; set => motDePasse = value; }
+    }
+}
