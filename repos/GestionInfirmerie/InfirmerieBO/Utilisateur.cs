1fb9aa841e8c7cba31be9e482926806320c719a1
Marcus <101703473+marcusWeb04@users.noreply.github.com>
2023-11-09 08:58:57 +0100

    BO avec utilisateur

---
+++ repos/GestionInfirmerie/InfirmerieBO/Utilisateur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfirmerieBO
+{
+    public class Utilisateur
+    {
+        //variable
+        string nom;
+        string mot_de_passe;
+
+        public Utilisateur(string nom, string mot_de_passe)
+        {
+            this.Nom = nom;
+            this.Mot_de_passe = mot_de_passe;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Mot_de_passe { get => mot_de_passe; set => mot_de_passe = value; }
+    }
+}
