4194fe008995303bb889f45f3e1ccd87dc6592e6
Snekye <93683127+Snekye@users.noreply.github.com>
2023-11-11 14:42:42 +0100

    Commentaires

--- repos/Infirmerie/InfirmerieBO/Utilisateur.cs
+++ repos/Infirmerie/InfirmerieBO/Utilisateur.cs
@@ -8,16 +8,18 @@ namespace InfirmerieBO
 {
     public class Utilisateur
     {
-        //attribut
+        // Attributs
         private string utilisateur_login;
         private string utilisateur_mdp;
 
+        // Constructeur
         public Utilisateur(string utilisateur_login, string utilisateur_mdp)
         {
             this.utilisateur_login = utilisateur_login;
             this.utilisateur_mdp = utilisateur_mdp;
         }
 
+        // Getter/Setters
         public string login { get => utilisateur_login; set => utilisateur_login = value; }
         public string mdp { get => utilisateur_mdp; set => utilisateur_mdp = value; }
     }
