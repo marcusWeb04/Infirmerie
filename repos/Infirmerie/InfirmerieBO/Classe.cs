2cb4135c1dd5d96f020775e4997a0e61651dcbf9
Marcus <101703473+marcusWeb04@users.noreply.github.com>
2023-11-16 09:47:55 +0100

    Ajout de la classe "Classe"
    Ajout du constructeur,
    des getters,
    des setters

---
+++ repos/Infirmerie/InfirmerieBO/Classe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfirmerieBO
+{
+    class Classe
+    {
+        int classe_id;
+        string classe_lib;
+
+        public Classe(int classe_id, string classe_lib)
+        {
+            this.classe_id = classe_id;
+            this.classe_lib = classe_lib;
+        }
+
+        public int Classe_id { get => classe_id; set => classe_id = value; }
+        public string Classe_lib { get => classe_lib; set => classe_lib = value; }
+
+    }
+}
