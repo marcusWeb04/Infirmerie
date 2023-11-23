622d9978ee4d9ba5be81f4a5f3a2e46783108e37
Marcus <101703473+marcusWeb04@users.noreply.github.com>
2023-11-23 11:21:04 +0100

    Ajout de la liste des médicaments
    Liste des médicaments fonctionnel ;)

---
+++ repos/Infirmerie/InfirmerieGUI/ListeMedicament.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace InfirmerieGUI
+{
+    partial class ListeMedicament
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonRetour = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.labelBienvenue = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.panel1.Controls.Add(this.buttonRetour);
+            this.panel1.Controls.Add(this.dgv);
+            this.panel1.Controls.Add(this.labelBienvenue);
+            this.panel1.Location = new System.Drawing.Point(103, 92);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1235, 739);
+            this.panel1.TabIndex = 12;
+            // 
+            // buttonRetour
+            // 
+            this.buttonRetour.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(168)))), ((int)(((byte)(134)))));
+            this.buttonRetour.FlatAppearance.BorderSize = 0;
+            this.buttonRetour.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRetour.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRetour.ForeColor = System.Drawing.Color.White;
+            this.buttonRetour.Location = new System.Drawing.Point(0, 659);
+            this.buttonRetour.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonRetour.Name = "buttonRetour";
+            this.buttonRetour.Size = new System.Drawing.Size(323, 80);
+            this.buttonRetour.TabIndex = 14;
+            this.buttonRetour.Text = "Retour";
+            this.buttonRetour.UseVisualStyleBackColor = false;
+            this.buttonRetour.Click += new System.EventHandler(this.buttonRetour_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeColumns = false;
+            this.dgv.AllowUserToResizeRows = false;
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(49)))), ((int)(((byte)(104)))));
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(37, 140);
+            this.dgv.MultiSelect = false;
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv.Size = new System.Drawing.Size(1164, 399);
+            this.dgv.TabIndex = 0;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
+            // 
+            // labelBienvenue
+            // 
+            this.labelBienvenue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.labelBienvenue.Font = new System.Drawing.Font("Arial", 31.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBienvenue.ForeColor = System.Drawing.Color.Black;
+            this.labelBienvenue.Location = new System.Drawing.Point(3, 11);
+            this.labelBienvenue.Name = "labelBienvenue";
+            this.labelBienvenue.Size = new System.Drawing.Size(1229, 58);
+            this.labelBienvenue.TabIndex = 10;
+            this.labelBienvenue.Text = "Médicament";
+            this.labelBienvenue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ListeMedicament
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1422, 977);
+            this.Controls.Add(this.panel1);
+            this.Name = "ListeMedicament";
+            this.Text = "ListeMedicament";
+            this.Load += new System.EventHandler(this.ListeMedicament_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonRetour;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label labelBienvenue;
+    }
+}
---
+++ repos/Infirmerie/InfirmerieGUI/ListeMedicament.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using InfirmerieBLL;
+using InfirmerieBO;
+
+
+namespace InfirmerieGUI
+{
+    public partial class ListeMedicament : Form
+    {
+        public static List<InfirmerieBO.Medicament> global_medicaments;
+        public ListeMedicament(string cond)
+        {
+            InitializeComponent();
+
+
+            //Confirmation de la chaine de connexion à la Base de données via la BLL
+
+            ConnexionBLL.SetchaineConnexion(ConfigurationManager.ConnectionStrings["Infirmerie"]);
+
+            //Colonne ID Cachée
+            DataGridViewTextBoxColumn LibColumn = new DataGridViewTextBoxColumn();
+            LibColumn.HeaderText = "Nom du médicament";
+            LibColumn.DataPropertyName = "lib";
+            LibColumn.Name = "Libele";
+
+            //Colonne ID Cachée
+            DataGridViewTextBoxColumn NbColumn = new DataGridViewTextBoxColumn();
+            NbColumn.HeaderText = "Nombre d'utilisation";
+            NbColumn.DataPropertyName = "nb";
+            NbColumn.Name = "nb";
+
+            // Créez une colonne de bouton personnalisée
+            DataGridViewButtonColumn buttonColumn = new DataGridViewButtonColumn();
+            buttonColumn.HeaderText = "Action";
+            buttonColumn.Text = "voir";
+            buttonColumn.UseColumnTextForButtonValue = true; // Affiche le texte du bouton dans chaque cellule
+
+            //Colonne ID Cachée
+            DataGridViewTextBoxColumn IdColumn = new DataGridViewTextBoxColumn();
+            IdColumn.DataPropertyName = "id";
+            IdColumn.Visible = false;
+            IdColumn.Name = "id";
+
+            // Ajout des 5 en-têtes de colonne au datagridview
+
+            dgv.Columns.Add(LibColumn);
+            dgv.Columns.Add(NbColumn);
+            dgv.Columns.Add(buttonColumn);
+            dgv.Columns.Add(IdColumn);
+
+            // Définition du style apporté au datagridview
+            dgv.ColumnHeadersVisible = true;
+            DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle();
+            columnHeaderStyle.BackColor = Color.Gray;
+            columnHeaderStyle.Font = new Font("Regular", 10, FontStyle.Bold);
+            dgv.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
+
+            global_medicaments = ConnexionBLL.getMedicaments(cond);
+            dgv.DataSource = global_medicaments;
+        }
+
+        private void ListeMedicament_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRetour_Click(object sender, EventArgs e)
+        {
+            //Affichage du formulaire de la recherche d'élèves
+            this.Hide();
+            Eleve Eleve = new Eleve();
+            Eleve.ShowDialog();
+        }
+
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
