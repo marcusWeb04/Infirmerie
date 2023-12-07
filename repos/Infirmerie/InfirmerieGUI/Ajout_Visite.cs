dbe78f4e748e03b31c54030079cc60eafad53631
Snekye <93683127+Snekye@users.noreply.github.com>
2023-12-07 15:53:40 +0100

    Correction d'un bug avec le champ qte_medic

--- repos/Infirmerie/InfirmerieGUI/Ajout_Visite.cs
+++ repos/Infirmerie/InfirmerieGUI/Ajout_Visite.cs
@@ -18,18 +18,29 @@ namespace InfirmerieGUI
         private static InfirmerieBO.Eleve global_el;
         public Ajout_Visite(InfirmerieBO.Eleve el)
         {
+            //Initialistion des données
             InitializeComponent();
             textBoxNom.Text = el.nom;
             textBoxPrenom.Text = el.prenom;
             textBoxClasse.Text = el.classe.lib;
             textBoxAge.Text = el.naiss;
-            comboBoxMedic.DataSource = ConnexionBLL.getMedicaments("");
+
+            //Ajout d'un médicament 'Aucun' 
+            InfirmerieBO.Medicament nomed = new InfirmerieBO.Medicament(0,"Aucun", 0);
+            List<InfirmerieBO.Medicament> nomedlist = new List<InfirmerieBO.Medicament>();
+            nomedlist.Add(nomed);
+
+            //Génération de la liste des médicaments
+            comboBoxMedic.DataSource = nomedlist.Concat(ConnexionBLL.getMedicaments("")).ToList();
+            comboBoxMedic.SelectedIndex = 0;
+            
             comboBoxSuite.SelectedIndex = 0;
             global_el = el;
         }
 
         private void buttonDummyAdd_Click(object sender, EventArgs e)
         {
+            //Récupération des données saisies
             string date = textBoxDate.Text;
             string arrivee = textBoxHeureArrivee.Text;
             string depart = textBoxHeureDepart.Text;
@@ -39,7 +50,16 @@ namespace InfirmerieGUI
 
             InfirmerieBO.Medicament medic = comboBoxMedic.SelectedItem as InfirmerieBO.Medicament;
 
-            string qte = textBoxQteMedic.Text;
+            int? qte;
+            if (textBoxQteMedic.Text == "")
+            {
+                qte = null;
+            }
+            else
+            {
+                qte = Int32.Parse(textBoxQteMedic.Text);
+            }
+
             bool parentsprev = checkBoxParentsPrev.Checked;
 
             if (date == "" || arrivee == "" || depart == "" || motif == "")
@@ -48,7 +68,14 @@ namespace InfirmerieGUI
             }
             else
             {
-                InfirmerieBO.Visite vi = new InfirmerieBO.Visite(0, global_el, medic, Int32.Parse(qte), date, arrivee, depart, motif, comm, parentsprev, suite, null);
+                //Si le médicament "Aucun" est séléctionné, on le met à null pour l'insertion
+                if (medic.id == 0) 
+                {
+                    medic = null;
+                }
+
+
+                InfirmerieBO.Visite vi = new InfirmerieBO.Visite(0, global_el, medic, qte, date, arrivee, depart, motif, comm, parentsprev, suite, null);
                 if (ConnexionBLL.addVisite(vi))
                 {
                     MessageBox.Show("Visite ajoutée!");
