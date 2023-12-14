34cb685b403a21aef598381d4c97a51f460f72d6
Snekye <93683127+Snekye@users.noreply.github.com>
2023-12-14 08:49:18 +0100

    Ajout des fonctions de verif de nom/tel

--- repos/Infirmerie/InfirmerieBLL/Connexion.cs
+++ repos/Infirmerie/InfirmerieBLL/Connexion.cs
@@ -31,14 +31,15 @@ namespace InfirmerieBLL
         }
 
         //Référence à la couche DAL
-            
+
         //Utilisateur
         public static bool GetUtilisateur(Utilisateur uti)
         {
             return ConnexionBDD.GetUtilisateur(uti);
         }
 
-        //Elève   
+        //Elève
+        
         public static bool addEleve(Eleve el)
         {
             return ConnexionBDD.addEleve(el);
@@ -79,6 +80,7 @@ namespace InfirmerieBLL
             return ConnexionBDD.getMedicaments(cond);
         }
 
+        //Visite
 
         public static bool addVisite(Visite vi)
         {
@@ -104,6 +106,61 @@ namespace InfirmerieBLL
         public static List<Visite> getVisitesNom(string cond)
         {
             return ConnexionBDD.getVisitesNom(cond);
+        }
+
+        //Fonctions supplémentaires
+
+        public static bool validateName(string name)
+        {
+            List<char> chars = new List<char>();
+            for (int i = 65; i <= 90; i++)
+            {
+                chars.Add((char)i);
+                chars.Add((char)(i + 32));
+            }
+            chars.Concat(new List<char>(){' ','-','\'','é','è','ê','ë','É','È','Ê','Ë'});
+
+            foreach (char c in name)
+            {
+                if (!chars.Contains(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static bool validatePhone(string phone)
+        {
+            //Formats acceptés : 
+            // 0688888888
+            // 06 88 88 88 88
+            // +33688888888
+            // +336 88 88 88 88
+            // 0033688888888
+            // 00 33 6 88 88 88 88
+
+            List<char> chars = new List<char>() {' ','+','0','1','2','3','4','5','6','7','8','9'};
+            int length = phone.Length;
+
+            foreach (char c in phone)
+            {
+                if (!chars.Contains(c))
+                {
+                    return false;
+                }
+                if (c == ' ' || c == '+')
+                {
+                    length--;
+                }
+            }
+            if (length > 14 || length < 10)
+            //Minimum : 10 (06 88 88 88 88)
+            //Maxiumum : 13 (+123 6 88 88 88 88
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
